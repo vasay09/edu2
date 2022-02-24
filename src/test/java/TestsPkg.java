b9fbd3b0426af17897ada1844a6040af6703e9da
vasay09 <s990@ya.ru>
2022-02-24 14:20:11 +0300

    321

--- src/test/java/TestsPkg.java
+++ src/test/java/TestsPkg.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class TestsPkg {
-    private static final Logger log = Logger.getLogger(TestsPkg.class);
+    private static final Logger log = Logger.getLogger(TestsPkg.class); //
 
 //    @Test
 //    public void testGettingKeysFromJsonFile() throws IOException, SQLException {
